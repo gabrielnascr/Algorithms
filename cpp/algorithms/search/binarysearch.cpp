c1f3bdf9b16465153db7e9df746a7cb07e3a5d57
gabrielnascr <gr153tt@gmail.com>
2020-10-06 20:19:29 -0300

    Implementing binary search in c ++

---
+++ cpp/algorithms/search/binarysearch.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int search(int size, int array[], int value)
+{
+    int begin = 0;
+    int end = size - 1;
+    int middle;
+
+    while (begin <= end)
+    {
+        middle = (begin + end) / 2;
+
+        if (value < array[middle])
+        {
+            end = middle - 1;
+        }
+        else if (value > array[middle])
+        {
+            begin = middle + 1;
+        }
+        else
+        {
+            return middle;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int value;
+
+    int array[] = {1, 4, 54, 87, 94, 21, 36, 45, 87, 98};
+    int size = (sizeof(array) / sizeof(int));
+
+    cout << "Number to search: " << endl;
+    cin >> value;
+
+    int result = search(size, array, value);
+
+    if (result >= 0)
+    {
+        cout << "The value " << array[result] << " was found";
+    }
+    else
+    {
+        cout << "The value " << value << " was not found";
+    }
+}
